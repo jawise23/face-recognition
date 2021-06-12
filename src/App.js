f65e87e7a102f2c3eead8d90689ada143b661037
James.Wise <james.wise@adm.com>
2021-06-12 01:34:09 -0400

    Changed to React Hook

--- src/App.js
+++ src/App.js
@@ -1,61 +1,52 @@
-import React, { Component } from "react";
-import Clarifai from "clarifai";
+import React, { useState, useEffect } from "react";
 import Particles from "react-particles-js";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
-import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import "./App.css";
 
-const app = new Clarifai.App({
-  apiKey: "45c32fcabcaf4772a7d62e982dd820d3",
-});
-
+// Using react-particles-js to render moving coolness
 const particlesOptions = {
-  particles: {
-    number: {
-      value: 75,
-      density: {
-        enable: true,
-        value_area: 800,
-      },
-    },
-  },
+	particles: {
+		number: {
+			value: 75,
+			density: {
+				enable: true,
+				value_area: 800,
+			},
+		},
+	},
 };
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      input: "",
-      imageUrl: "",
-    };
-  }
-  //https://www.gardeningknowhow.com/wp-content/uploads/2020/12/lonely-japanese-cherry-400x300.jpg
-  onInputChange = (event) => {
-    this.setState({ input: event.target.value });
-  };
+function App() {
+	// Deconstructing useState into the state variable and the setState
+	const [input, setInput] = useState("");
+	const [imageUrl, setImageUrl] = useState("");
 
-  onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
-  };
+	// Creating variables looking for event changes.
+	const onInputChange = (event) => {
+		setInput(event.target.value);
+	};
+	const onButtonSubmit = (event) => {
+		setImageUrl(input);
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <Particles className="particles" params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
-      </div>
-    );
-  }
+	// Return the App
+	return (
+		<div className="App">
+			<Particles className="particles" params={particlesOptions} />
+			<Navigation />
+			<Logo />
+			<Rank />
+			<ImageLinkForm
+				onInputChange={onInputChange}
+				onButtonSubmit={onButtonSubmit}
+			/>
+			<FaceRecognition imageUrl={imageUrl} />
+		</div>
+	);
 }
 
 export default App;
